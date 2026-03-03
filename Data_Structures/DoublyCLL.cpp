1b82c91cdf1602fac93862aacda223fb57ec5d7f
UmeshBhabad <umeshbhabad9@gmail.com>
2026-03-04 01:45:28 +0530

    Added Doubly Circular Linked List

--- Data_Structures/DoublyCLL.cpp
+++ Data_Structures/DoublyCLL.cpp
@@ -1,14 +1,21 @@
+///////////////////////////////////////////////////////////////
+// Final code of Doubly Linear Linked List using Generic approach
+///////////////////////////////////////////////////////////////
+
 # include <iostream>
 using namespace std;
 
+# pragma pack(1)
+
+template <class T>
 class DoublyCLLnode
 {
     public:
-        int data;
-        DoublyCLLnode *next;
-        DoublyCLLnode *prev;
+        T data;
+        DoublyCLLnode<T> *next;
+        DoublyCLLnode<T> *prev;
 
-        DoublyCLLnode(int No)
+        DoublyCLLnode(T No)
         {
             this -> data = No;
             this -> next = NULL;
@@ -16,29 +23,267 @@ class DoublyCLLnode
         }
 };
 
+template <class T>
 class DoublyCLL
 {
     public:
-        DoublyCLLnode *first;
-        DoublyCLLnode *last;
+        DoublyCLLnode<T> *first;
+        DoublyCLLnode<T> *last;
         int iCount;
 
-        DoublyCLL()
+        DoublyCLL();
+
+        void InsertFirst(T);
+        void InsertLast(T);
+        void InsertAtPos(T, int);
+
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int);
+
+        void Display();
+        int Count();
+};
+
+template <class T>
+DoublyCLL<T> :: DoublyCLL()
+{
+    this -> first = NULL;
+    this -> last = NULL;
+    this -> iCount = 0; 
+}
+
+template <class T>
+void DoublyCLL<T> :: InsertFirst(T No)
+{
+    DoublyCLLnode<T> *newn = new DoublyCLLnode<T>(No);
+
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        this -> first = newn;
+        this -> last = newn;
+    }
+    else
+    {
+        newn -> next = this -> first;
+        this -> first -> prev = newn;
+        this -> first = newn;
+    }
+    this -> last -> next = this -> first;
+    this -> first -> prev = this -> last;
+    this -> iCount++;
+}
+
+template <class T>
+void DoublyCLL<T> :: InsertLast(T No)
+{
+    DoublyCLLnode<T> *newn = new DoublyCLLnode<T>(No);
+
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        this -> first = newn;
+        this -> last = newn;
+    }
+    else
+    {
+        this -> last -> next = newn;
+        newn -> prev = this -> last;
+        this -> last = newn;
+    }
+    this -> last -> next = this -> first;
+    this -> first -> prev = this -> last;
+    this -> iCount++;
+}
+
+template <class T>
+void DoublyCLL<T> :: InsertAtPos(T No, int pos)
+{
+    if(pos < 1 || pos > this -> iCount + 1)
+    {
+        cout << "Invalid Position\n";
+        return;
+    }
+
+    if(pos == 1)
+    {
+        this -> InsertFirst(No);
+    }
+    else if(pos == this -> iCount + 1)
+    {
+        this -> InsertLast(No);
+    }
+    else
+    {
+        DoublyCLLnode<T> *newn = new DoublyCLLnode<T>(No);
+        DoublyCLLnode<T> *temp = NULL;
+
+        int iCnt = 0;
+
+        temp = this -> first;
+        for(iCnt = 1; iCnt < pos - 1; iCnt++)
         {
-            this -> first = NULL;
-            this -> last = NULL;
-            this -> iCount = 0; 
+            temp = temp -> next;
+        }
+        newn -> next = temp -> next;
+        newn -> next -> prev = newn;
+        newn -> prev = temp;
+        temp -> next = newn;
+
+        this -> iCount++;
+    }
+}
+
+template <class T>
+void DoublyCLL<T> :: DeleteFirst()
+{
+    DoublyCLLnode<T> *temp = NULL;
+    
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        return;
+    }
+    else if(this -> first == this -> last)
+    {
+        delete first;
+        this -> first = NULL;
+        this -> last = NULL;
+    }
+    else
+    {
+        this -> first = this -> first -> next;
+        delete last -> next;
+    }
+    this -> last -> next = this -> first;
+    this -> first -> prev = this -> last;
+
+    this -> iCount--;
+}
+
+template <class T>
+void DoublyCLL<T> :: DeleteLast()
+{
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        return;
+    }
+    else if(this -> first == this -> last)
+    {
+        delete first;
+        this -> first = NULL;
+        this -> last = NULL;
+    }
+    else
+    {
+        this -> last = this -> last -> prev;
+        delete this -> last -> next;
+    }
+    this -> last -> next = this -> first;
+    this -> first -> prev = this -> last;
+
+    this -> iCount--;
+}
+
+template <class T>
+void DoublyCLL<T> :: DeleteAtPos(int pos)
+{
+    if(pos < 1 || pos > this -> iCount)
+    {
+        cout << "Invalid Position\n";
+        return;
+    }
+
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        this -> DeleteFirst();
+    }
+    else if(this -> first == this -> last)
+    {
+        this -> DeleteLast();
+    }
+    else
+    {
+        DoublyCLLnode<T> *temp = NULL;
+        DoublyCLLnode<T> *target = NULL;
+
+        temp = this -> first;
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt < pos - 1; iCnt++)
+        {
+            temp = temp -> next;
         }
 
-        void InsertFirst(int No)
-        {
-            
-        }
-};
+        target = temp -> next;
+
+        temp -> next = target -> next;
+        temp -> next -> prev = temp;
+        delete target;
+
+        this -> iCount--;
+    }
+}
+
+template <class T>
+void DoublyCLL<T> :: Display()
+{
+    DoublyCLLnode<T> *temp = NULL;
+
+    temp = first;
+
+    cout << "\n<=> ";
+    do
+    {
+        cout << "| " << temp -> data << " | <=> ";
+        temp = temp -> next;
+    }while(temp != first);
+    cout << "\n";
+}
+
+template <class T>
+int DoublyCLL<T> :: Count()
+{
+    return this -> iCount;
+}
 
 int main()
 {
-    DoublyCLL *dcobj = new DoublyCLL(); 
+    DoublyCLL<int> *dcobj = new DoublyCLL<int>();
+
+    dcobj -> InsertFirst(51);
+    dcobj -> InsertFirst(21);
+    dcobj -> InsertFirst(11);
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> InsertLast(101);
+    dcobj -> InsertLast(121);
+    dcobj -> InsertLast(151);
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> InsertAtPos(111, 5);
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> DeleteFirst();
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> DeleteLast();
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> DeleteAtPos(4);
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    delete dcobj;    
 
     return 0;
 }
--- stdds.cpp
+++ stdds.cpp
@@ -9,7 +9,7 @@
     Singly Linear               SinglyLLLnode                       SinglyLLL       Done
     Singly Circular             SinglyCLLnode                       SinglyCLL       Done
     Doubly Linear               DoublyLLLnode                       DoublyLLL       Done
-    Doubly Circular             DoublyCLLnode                       DoublyCLL
+    Doubly Circular             DoublyCLLnode                       DoublyCLL       Done
     Stack                       Stacknode                           Stack           Done
     Queue                       Queuenode                           Queue           Done
     BST                             -                               
@@ -52,12 +52,14 @@ class SinglyLLL
 
         void InsertFirst(T);
         void InsertLast(T);
+        void InsertAtPos(T, int);
+
         void DeleteFirst();
         void DeleteLast();
+        void DeleteAtPos(int);
+        
         void Display();
         int Count();
-        void InsertAtPos(T, int);
-        void DeleteAtPos(int);
 };
 
 template<class T>
@@ -784,6 +786,246 @@ int DoublyLLL <T> :: Count()
 //          Doubly Circular Linked List using Generic Approach
 /////////////////////////////////////////////////////////////////////////
 
+# pragma pack(1)
+
+template <class T>
+class DoublyCLLnode
+{
+    public:
+        T data;
+        DoublyCLLnode<T> *next;
+        DoublyCLLnode<T> *prev;
+
+        DoublyCLLnode(T No)
+        {
+            this -> data = No;
+            this -> next = NULL;
+            this -> prev = NULL;
+        }
+};
+
+template <class T>
+class DoublyCLL
+{
+    public:
+        DoublyCLLnode<T> *first;
+        DoublyCLLnode<T> *last;
+        int iCount;
+
+        DoublyCLL();
+
+        void InsertFirst(T);
+        void InsertLast(T);
+        void InsertAtPos(T, int);
+
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int);
+
+        void Display();
+        int Count();
+};
+
+template <class T>
+DoublyCLL<T> :: DoublyCLL()
+{
+    this -> first = NULL;
+    this -> last = NULL;
+    this -> iCount = 0; 
+}
+
+template <class T>
+void DoublyCLL<T> :: InsertFirst(T No)
+{
+    DoublyCLLnode<T> *newn = new DoublyCLLnode<T>(No);
+
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        this -> first = newn;
+        this -> last = newn;
+    }
+    else
+    {
+        newn -> next = this -> first;
+        this -> first -> prev = newn;
+        this -> first = newn;
+    }
+    this -> last -> next = this -> first;
+    this -> first -> prev = this -> last;
+    this -> iCount++;
+}
+
+template <class T>
+void DoublyCLL<T> :: InsertLast(T No)
+{
+    DoublyCLLnode<T> *newn = new DoublyCLLnode<T>(No);
+
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        this -> first = newn;
+        this -> last = newn;
+    }
+    else
+    {
+        this -> last -> next = newn;
+        newn -> prev = this -> last;
+        this -> last = newn;
+    }
+    this -> last -> next = this -> first;
+    this -> first -> prev = this -> last;
+    this -> iCount++;
+}
+
+template <class T>
+void DoublyCLL<T> :: InsertAtPos(T No, int pos)
+{
+    if(pos < 1 || pos > this -> iCount + 1)
+    {
+        cout << "Invalid Position\n";
+        return;
+    }
+
+    if(pos == 1)
+    {
+        this -> InsertFirst(No);
+    }
+    else if(pos == this -> iCount + 1)
+    {
+        this -> InsertLast(No);
+    }
+    else
+    {
+        DoublyCLLnode<T> *newn = new DoublyCLLnode<T>(No);
+        DoublyCLLnode<T> *temp = NULL;
+
+        int iCnt = 0;
+
+        temp = this -> first;
+        for(iCnt = 1; iCnt < pos - 1; iCnt++)
+        {
+            temp = temp -> next;
+        }
+        newn -> next = temp -> next;
+        newn -> next -> prev = newn;
+        newn -> prev = temp;
+        temp -> next = newn;
+
+        this -> iCount++;
+    }
+}
+
+template <class T>
+void DoublyCLL<T> :: DeleteFirst()
+{
+    DoublyCLLnode<T> *temp = NULL;
+    
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        return;
+    }
+    else if(this -> first == this -> last)
+    {
+        delete first;
+        this -> first = NULL;
+        this -> last = NULL;
+    }
+    else
+    {
+        this -> first = this -> first -> next;
+        delete last -> next;
+    }
+    this -> last -> next = this -> first;
+    this -> first -> prev = this -> last;
+
+    this -> iCount--;
+}
+
+template <class T>
+void DoublyCLL<T> :: DeleteLast()
+{
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        return;
+    }
+    else if(this -> first == this -> last)
+    {
+        delete first;
+        this -> first = NULL;
+        this -> last = NULL;
+    }
+    else
+    {
+        this -> last = this -> last -> prev;
+        delete this -> last -> next;
+    }
+    this -> last -> next = this -> first;
+    this -> first -> prev = this -> last;
+
+    this -> iCount--;
+}
+
+template <class T>
+void DoublyCLL<T> :: DeleteAtPos(int pos)
+{
+    if(pos < 1 || pos > this -> iCount)
+    {
+        cout << "Invalid Position\n";
+        return;
+    }
+
+    if(this -> first == NULL && this -> last == NULL)
+    {
+        this -> DeleteFirst();
+    }
+    else if(this -> first == this -> last)
+    {
+        this -> DeleteLast();
+    }
+    else
+    {
+        DoublyCLLnode<T> *temp = NULL;
+        DoublyCLLnode<T> *target = NULL;
+
+        temp = this -> first;
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt < pos - 1; iCnt++)
+        {
+            temp = temp -> next;
+        }
+
+        target = temp -> next;
+
+        temp -> next = target -> next;
+        temp -> next -> prev = temp;
+        delete target;
+
+        this -> iCount--;
+    }
+}
+
+template <class T>
+void DoublyCLL<T> :: Display()
+{
+    DoublyCLLnode<T> *temp = NULL;
+
+    temp = first;
+
+    cout << "\n<=> ";
+    do
+    {
+        cout << "| " << temp -> data << " | <=> ";
+        temp = temp -> next;
+    }while(temp != first);
+    cout << "\n";
+}
+
+template <class T>
+int DoublyCLL<T> :: Count()
+{
+    return this -> iCount;
+}
+
 /////////////////////////////////////////////////////////////////////////
 //          Stack using Generic Approach
 /////////////////////////////////////////////////////////////////////////
@@ -1019,7 +1261,7 @@ void Queue<T> :: Display()
 
     while(temp != NULL)
     {
-        cout << "| " << temp -> data << "| - ";
+        cout << "| " << temp -> data << " | - ";
         temp = temp -> next; 
     }
     cout<< "\n";
@@ -1035,6 +1277,10 @@ int Queue<T> :: Count()
 
 int main()
 {
+    cout << "---------------------------------------------------------------------------------\n";
+    cout << "Singly Linear LL\n";
+    cout << "---------------------------------------------------------------------------------\n";
+
     SinglyLLL<int> *slobj = new SinglyLLL<int>();
     int iRet = 0;
 
@@ -1086,7 +1332,13 @@ int main()
 
     delete slobj;
 
+    cout << "---------------------------------------------------------------------------------\n";
+
     //////////////////////////////////////////////////////////////////////////////
+
+    cout << "---------------------------------------------------------------------------------\n";
+    cout << "Singly Circular LL\n";
+    cout << "---------------------------------------------------------------------------------\n";
 
     SinglyCLLL<int> *scobj = new SinglyCLLL<int>();
 
@@ -1126,53 +1378,111 @@ int main()
 
     delete scobj;
 
+    cout << "---------------------------------------------------------------------------------\n";
+
     //////////////////////////////////////////////////////////////////////////////
 
-    DoublyLLL <char>*dobj = new DoublyLLL<char>();
+    cout << "---------------------------------------------------------------------------------\n";
+    cout << "Doubly Linear LL\n";
+    cout << "---------------------------------------------------------------------------------\n";
 
-    dobj -> InsertFirst('A');
-    dobj -> InsertFirst('B');
-    dobj -> InsertFirst('C');
+    DoublyLLL <char>*dlobj = new DoublyLLL<char>();
 
-    dobj -> Display();
+    dlobj -> InsertFirst('A');
+    dlobj -> InsertFirst('B');
+    dlobj -> InsertFirst('C');
 
-    cout<< "Number of elements are : "<< dobj -> Count() << "\n";
+    dlobj -> Display();
 
-    dobj -> InsertLast('X');
-    dobj -> InsertLast('Y');
-    dobj -> InsertLast('Z');
+    cout<< "Number of elements are : "<< dlobj -> Count() << "\n";
 
-    dobj -> Display();
+    dlobj -> InsertLast('X');
+    dlobj -> InsertLast('Y');
+    dlobj -> InsertLast('Z');
 
-    cout<< "Number of elements are : "<< dobj -> Count() << "\n";
+    dlobj -> Display();
 
-    dobj -> DeleteFirst();
+    cout<< "Number of elements are : "<< dlobj -> Count() << "\n";
 
-    dobj -> Display();
+    dlobj -> DeleteFirst();
 
-    cout<< "Number of elements are : "<< dobj -> Count() << "\n";
+    dlobj -> Display();
 
-    dobj -> DeleteLast();
+    cout<< "Number of elements are : "<< dlobj -> Count() << "\n";
 
-    dobj -> Display();
+    dlobj -> DeleteLast();
 
-    cout<< "Number of elements are : "<< dobj -> Count() << "\n";
+    dlobj -> Display();
 
-    dobj -> InsertAtPos('$',4);
+    cout<< "Number of elements are : "<< dlobj -> Count() << "\n";
 
-    dobj -> Display();
+    dlobj -> InsertAtPos('$',4);
 
-    cout<< "Number of elements are : "<< dobj -> Count() << "\n";
+    dlobj -> Display();
 
-    dobj -> DeleteAtPos(4);
+    cout<< "Number of elements are : "<< dlobj -> Count() << "\n";
 
-    dobj -> Display();
+    dlobj -> DeleteAtPos(4);
 
-    cout<< "Number of elements are : "<< dobj -> Count() << "\n";
+    dlobj -> Display();
 
-    delete dobj;
+    cout<< "Number of elements are : "<< dlobj -> Count() << "\n";
+
+    delete dlobj;
+
+    cout << "---------------------------------------------------------------------------------\n";
 
     ////////////////////////////////////////////////////////////////////////////
+
+    cout << "---------------------------------------------------------------------------------\n";
+    cout << "Doubly Circular LL\n";
+    cout << "---------------------------------------------------------------------------------\n";
+
+    DoublyCLL<int> *dcobj = new DoublyCLL<int>();
+
+    dcobj -> InsertFirst(51);
+    dcobj -> InsertFirst(21);
+    dcobj -> InsertFirst(11);
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> InsertLast(101);
+    dcobj -> InsertLast(121);
+    dcobj -> InsertLast(151);
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> InsertAtPos(111, 5);
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> DeleteFirst();
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> DeleteLast();
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    dcobj -> DeleteAtPos(4);
+
+    dcobj -> Display();
+    cout << "Number of nodes : " << dcobj -> Count() << "\n";
+
+    delete dcobj;
+
+    cout << "---------------------------------------------------------------------------------\n";
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    cout << "---------------------------------------------------------------------------------\n";
+    cout << "Stack\n";
+    cout << "---------------------------------------------------------------------------------\n";
 
     Stack <char>*sobj = new Stack<char>();
 
@@ -1211,7 +1521,13 @@ int main()
 
     delete sobj;
 
+    cout << "---------------------------------------------------------------------------------\n";
+
     //////////////////////////////////////////////////////////////////////////////////
+
+    cout << "---------------------------------------------------------------------------------\n";
+    cout << "Queue\n";
+    cout << "---------------------------------------------------------------------------------\n";
 
     Queue<double> *qobj = new Queue<double>();
 
@@ -1243,6 +1559,8 @@ int main()
     cout << "Number of elements in Queue are : "<< qobj -> Count()<< "\n";
 
     delete qobj;
+
+    cout << "---------------------------------------------------------------------------------\n";
 
     //////////////////////////////////////////////////////////////////////////////
 
